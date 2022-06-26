9c2cf85620ca2b063b34c1e1c724ea3cbe788112
酒酒 <2188484122@qq.com>
2022-06-26 09:45:40 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入移动组件库
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 移动端适配
+import 'amfe-flexible'
 
 Vue.config.productionTip = false
+
+Vue.use(Vant)
 
 new Vue({
   router,
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = '就断了吧断了吧'
+        return args
+      })
+  }
 })
